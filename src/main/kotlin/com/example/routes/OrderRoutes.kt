85655f6080fe3fae399e91c03e47401c7f3cfa2e
yuuki1293 <yuuki1293@outlook.jp>
2022-11-28 19:54:36 +0900

    feat: create totalizeOrderRoute

--- src/main/kotlin/com/example/routes/OrderRoutes.kt
+++ src/main/kotlin/com/example/routes/OrderRoutes.kt
@@ -24,3 +24,15 @@ fun Route.getOrderRoute() {
         call.respond(order)
     }
 }
+
+fun Route.totalizeOrderRoute() {
+    get("/order/{id?}/total") {
+        val id = call.parameters["id"] ?: return@get call.respondText("Bad Request", status = HttpStatusCode.BadRequest)
+        val order = orderStorage.find { it.number == id } ?: return@get call.respondText(
+            "Not Found",
+            status = HttpStatusCode.NotFound
+        )
+        val total = order.contents.sumOf { it.price * it.amount }
+        call.respond(total)
+    }
+}
