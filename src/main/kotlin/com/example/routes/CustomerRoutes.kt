3ce9c02647241d18060b7c384a61d27cf4844769
yuuki1293 <yuuki1293@outlook.jp>
2022-11-28 19:12:52 +0900

    feat: /customer get request return customers data

--- src/main/kotlin/com/example/routes/CustomerRoutes.kt
+++ src/main/kotlin/com/example/routes/CustomerRoutes.kt
@@ -1,13 +1,21 @@
 package com.example.routes
 
+import com.example.models.customerStorage
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Route.customerRouting() {
     route("/customer") {
         get {
-
+            if (customerStorage.isNotEmpty()) {
+                call.respond(customerStorage)
+            } else {
+                call.respondText("No customers found", status = HttpStatusCode.OK)
+            }
         }
-        get("{id?}"){
+        get("{id?}") {
 
         }
         post {
