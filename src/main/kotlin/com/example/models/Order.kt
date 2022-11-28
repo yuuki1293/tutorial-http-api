4cbf26cca649118bdcde6d72efbd1549a89d5cb4
yuuki1293 <yuuki1293@outlook.jp>
2022-11-28 19:45:20 +0900

    feat: add Order

---
+++ src/main/kotlin/com/example/models/Order.kt
@@ -0,0 +1,9 @@
+package com.example.models
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Order(val number: String, val contents: List<OrderItem>)
+
+@Serializable
+data class OrderItem(val item: String, val amount: Int, val price: Double)
