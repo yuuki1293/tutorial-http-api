14a1d48ee46d11eb9339e27b2d79c5360db6c097
yuuki1293 <yuuki1293@outlook.jp>
2022-11-28 19:29:47 +0900

    feat: /customer/{id} delete request delete customer

--- src/main/kotlin/com/example/routes/CustomerRoutes.kt
+++ src/main/kotlin/com/example/routes/CustomerRoutes.kt
@@ -35,7 +35,12 @@ fun Route.customerRouting() {
             call.respondText("Customer stored currently", status = HttpStatusCode.Created)
         }
         delete("{id?}") {
-
+            val id = call.parameters["id"] ?: return@delete call.respond(HttpStatusCode.BadRequest)
+            if (customerStorage.removeIf { it.id == id }) {
+                call.respondText("Customer removed correctly", status = HttpStatusCode.Accepted)
+            } else {
+                call.respondText("Not Found", status = HttpStatusCode.NotFound)
+            }
         }
     }
 }
