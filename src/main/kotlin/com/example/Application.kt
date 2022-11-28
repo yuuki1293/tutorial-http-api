8a0deb420efe0a4e69956ba35274d83f545861c3
yuuki1293 <yuuki1293@outlook.jp>
2022-11-28 19:18:07 +0900

    fix: add routing

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -4,6 +4,7 @@ import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import com.example.plugins.*
+import com.example.routes.customerRouting
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,5 +1,6 @@
 package com.example.plugins
 
+import com.example.routes.customerRouting
 import io.ktor.server.routing.*
 import io.ktor.http.*
 import io.ktor.server.application.*
@@ -7,10 +8,10 @@ import io.ktor.server.response.*
 import io.ktor.server.request.*
 
 fun Application.configureRouting() {
-
     routing {
         get("/") {
             call.respondText("Hello World!")
         }
+        customerRouting()
     }
 }
