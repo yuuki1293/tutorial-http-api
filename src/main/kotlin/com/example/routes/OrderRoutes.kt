dbe9155de3d1bc47b138d7504a26c85b14583a6e
yuuki1293 <yuuki1293@outlook.jp>
2022-11-28 19:52:46 +0900

    feat: add OrderRoutes

---
+++ src/main/kotlin/com/example/routes/OrderRoutes.kt
@@ -0,0 +1,15 @@
+package com.example.routes
+
+import com.example.models.*
+import io.ktor.server.application.*
+import io.ktor.http.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Route.listOrdersRoute() {
+    get("/order") {
+        if (orderStorage.isNotEmpty()) {
+            call.respond(orderStorage)
+        }
+    }
+}
