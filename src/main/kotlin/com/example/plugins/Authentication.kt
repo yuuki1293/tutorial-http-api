ed36963451dd2d82a8925fdd7c00573a315aebaf
yuuki1293 <yuuki1293@outlook.jp>
2022-11-29 11:39:22 +0900

    feat: UserHashedTableAuth

--- src/main/kotlin/com/example/plugins/Authentication.kt
+++ src/main/kotlin/com/example/plugins/Authentication.kt
@@ -4,20 +4,25 @@ import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.util.*
 
 fun Application.configureAuthentication() {
+    val digestFunction = getDigestFunction("SHA-256") { "ktor${it.length}" }
+    val hashedUserTable = UserHashedTableAuth(
+        table = mapOf(
+            "jetbrains" to digestFunction("foobar"),
+            "admin" to digestFunction("password")
+        ),
+        digester = digestFunction
+    )
     install(Authentication) {
-        basic("auth-basic") {
+        basic("auth-basic-hashed") {
             realm = "Access to the '/auth-basic' path"
             validate { credentials ->
-                if (credentials.name == "jetbrains" && credentials.password == "foobar") {
-                    UserIdPrincipal(credentials.name)
-                } else {
-                    null
-                }
+                hashedUserTable.authenticate(credentials)
             }
         }
-	}
+    }
 
     routing {
         authenticate("auth-basic") {
