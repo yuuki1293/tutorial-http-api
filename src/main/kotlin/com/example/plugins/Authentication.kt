fabab963838d3b6af72983c73b5a301d6c1cd98d
yuuki1293 <yuuki1293@outlook.jp>
2022-11-29 12:11:22 +0900

    feat: auth digest

--- src/main/kotlin/com/example/plugins/Authentication.kt
+++ src/main/kotlin/com/example/plugins/Authentication.kt
@@ -6,8 +6,11 @@ import io.ktor.server.auth.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.util.*
+import java.security.MessageDigest
+import kotlin.text.Charsets.UTF_8
 
 fun Application.configureAuthentication() {
+    // region basic-hashed
     val digestFunction = getDigestFunction("SHA-256") { "ktor${it.length}" }
     val hashedUserTable = UserHashedTableAuth(
         table = mapOf(
@@ -16,6 +19,18 @@ fun Application.configureAuthentication() {
         ),
         digester = digestFunction
     )
+    // endregion
+
+    // region digest
+    fun getMd5Digest(str: String): ByteArray = MessageDigest.getInstance("MD5").digest(str.toByteArray(UTF_8))
+
+    val myRealm = "Access to the '/auth-digest' path"
+    val userTable: Map<String, ByteArray> = mapOf(
+        "jetbrains" to getMd5Digest("jetbrains:$myRealm:foobar"),
+        "admin" to getMd5Digest("admin:$myRealm:password")
+    )
+    // endregion
+
     install(Authentication) {
         basic("auth-basic-hashed") {
             realm = "Access to the '/auth-basic' path"
@@ -23,11 +38,23 @@ fun Application.configureAuthentication() {
                 hashedUserTable.authenticate(credentials)
             }
         }
+
+        digest("auth-digest") {
+            realm = myRealm
+            digestProvider { userName, realm ->
+                userTable[userName]
+            }
+        }
     }
 
     routing {
         authenticate("auth-basic-hashed") {
             get("/auth-basic") {
+                call.respondText("Hello, ${call.principal<UserIdPrincipal>()?.name}!")
+            }
+        }
+        authenticate("auth-digest") {
+            get("/auth-digest") {
                 call.respondText("Hello, ${call.principal<UserIdPrincipal>()?.name}!")
             }
         }
