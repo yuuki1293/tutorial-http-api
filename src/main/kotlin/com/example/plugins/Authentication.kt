880908b584ab06066128005ed69b9a32bc47296f
yuuki1293 <yuuki1293@outlook.jp>
2022-11-29 11:23:44 +0900

    feat: add Authentication

---
+++ src/main/kotlin/com/example/plugins/Authentication.kt
@@ -0,0 +1,10 @@
+package com.example.plugins
+
+import io.ktor.server.application.*
+import io.ktor.server.auth.*
+
+fun Application.configureAuthentication() {
+    install(Authentication) {
+
+	}
+}
