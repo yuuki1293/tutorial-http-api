bc5478d5900bbf588b2c263ca784745400eb1d31
yuuki1293 <yuuki1293@outlook.jp>
2022-11-29 10:56:55 +0900

    chore: optimize import

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,10 +1,10 @@
 package com.example
 
+import com.example.plugins.configureRouting
+import com.example.plugins.configureSerialization
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import com.example.plugins.*
-import com.example.routes.customerRouting
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
