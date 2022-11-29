aef279ba26a033847981d1d601a6eb6d7519aa4f
yuuki1293 <yuuki1293@outlook.jp>
2022-11-29 11:12:56 +0900

    fix: test pass

--- src/test/kotlin/com/example/OrderRouteTests.kt
+++ src/test/kotlin/com/example/OrderRouteTests.kt
@@ -1,14 +1,21 @@
 package com.example
 
+import com.example.plugins.configureRouting
+import com.example.plugins.configureSerialization
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.server.testing.*
-import kotlin.test.*
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class OrderRouteTests {
     @Test
     fun testGetOrder() = testApplication {
+        application {
+            configureRouting()
+            configureSerialization()
+        }
         val response = client.get("/order/2020-04-06-01")
         assertEquals(
             """{"number":"2020-04-06-01","contents":[{"item":"Ham Sandwich","amount":2,"price":5.5},{"item":"Water","amount":1,"price":1.5},{"item":"Beer","amount":3,"price":2.3},{"item":"Cheesecake","amount":1,"price":3.75}]}""",
