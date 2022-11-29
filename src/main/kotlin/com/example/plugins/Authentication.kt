b5ca2a1f500d619146c0f1505c6e081146910ce7
yuuki1293 <yuuki1293@outlook.jp>
2022-11-29 11:35:28 +0900

    add: create basic auth route

--- src/main/kotlin/com/example/plugins/Authentication.kt
+++ src/main/kotlin/com/example/plugins/Authentication.kt
@@ -2,11 +2,28 @@ package com.example.plugins
 
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 
 fun Application.configureAuthentication() {
     install(Authentication) {
         basic("auth-basic") {
-
+            realm = "Access to the '/auth-basic' path"
+            validate { credentials ->
+                if (credentials.name == "jetbrains" && credentials.password == "foobar") {
+                    UserIdPrincipal(credentials.name)
+                } else {
+                    null
+                }
+            }
         }
 	}
+
+    routing {
+        authenticate("auth-basic") {
+            get("/auth-basic") {
+                call.respondText("Hello, ${call.principal<UserIdPrincipal>()?.name}!")
+            }
+        }
+    }
 }
