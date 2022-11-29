240f90133aa184320455ab889c20075851bbe541
yuuki1293 <yuuki1293@outlook.jp>
2022-11-29 15:46:46 +0900

    test: auth form

--- src/test/kotlin/com/example/AuthenticationTests.kt
+++ src/test/kotlin/com/example/AuthenticationTests.kt
@@ -2,6 +2,7 @@ package com.example
 
 import com.example.plugins.configureAuthentication
 import io.ktor.client.request.*
+import io.ktor.client.request.forms.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.server.testing.*
@@ -19,6 +20,27 @@ class AuthenticationTests {
                 basicAuth("jetbrains", "foobar")
             }
         }
+        assertEquals(
+            "Hello, jetbrains!",
+            response.bodyAsText()
+        )
+        assertEquals(HttpStatusCode.OK, response.status)
+    }
+
+    @Test
+    fun testAuthForm() = testApplication {
+        application {
+            configureAuthentication()
+        }
+        val response = client.submitForm(
+            url = "/auth-form",
+            formParameters = Parameters.build {
+                append("username", "jetbrains")
+                append("password", "foobar")
+            }
+        )
+        println(response.status)
+        response.headers.forEach { a, b -> b.forEach { println(it + a) } }
         assertEquals(
             "Hello, jetbrains!",
             response.bodyAsText()
