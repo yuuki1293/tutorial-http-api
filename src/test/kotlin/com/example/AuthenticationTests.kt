1865e5322b4202b1f5ca0ecc37678631106c9b97
yuuki1293 <yuuki1293@outlook.jp>
2022-11-29 11:56:50 +0900

    fix: test pass

--- src/test/kotlin/com/example/AuthenticationTests.kt
+++ src/test/kotlin/com/example/AuthenticationTests.kt
@@ -1,5 +1,29 @@
 package com.example
 
-class AuthenticationTests {
+import com.example.plugins.configureAuthentication
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.server.testing.*
+import org.junit.Test
+import kotlin.test.assertEquals
 
+class AuthenticationTests {
+    @Test
+    fun testAuthBasic() = testApplication {
+        application {
+            configureAuthentication()
+        }
+        val response = client.get("/auth-basic") {
+            url {
+                basicAuth("jetbrains", "foobar")
+            }
+        }
+        println(response.request.url)
+        assertEquals(
+            "Hello, jetbrains!",
+            response.bodyAsText()
+        )
+        assertEquals(HttpStatusCode.OK, response.status)
+    }
 }
