a39779a5ee1f677061f26bb19fc41a2b77b6c78e
yuuki1293 <yuuki1293@outlook.jp>
2022-12-01 10:00:34 +0900

    jwtなんもわからん

--- src/main/kotlin/com/example/plugins/Authentication.kt
+++ src/main/kotlin/com/example/plugins/Authentication.kt
@@ -1,12 +1,23 @@
 package com.example.plugins
 
+import com.auth0.jwk.JwkProviderBuilder
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.util.*
+import java.security.KeyFactory
 import java.security.MessageDigest
+import java.security.interfaces.RSAPrivateKey
+import java.security.interfaces.RSAPublicKey
+import java.security.spec.PKCS8EncodedKeySpec
+import java.util.*
+import java.util.concurrent.TimeUnit
 import kotlin.text.Charsets.UTF_8
 
 fun Application.configureAuthentication() {
@@ -29,6 +40,25 @@ fun Application.configureAuthentication() {
         "jetbrains" to getMd5Digest("jetbrains:$myRealm:foobar"),
         "admin" to getMd5Digest("admin:$myRealm:password")
     )
+    // endregion
+
+    // region jwt
+    val privateKeyString = environment.config.property("jwt.privateKey").getString()
+    val issuer = environment.config.property("jwt.issuer").getString()
+    val audience = environment.config.property("jwt.audience").getString()
+    val jwtRealm = environment.config.property("jwt.realm").getString()
+    val jwkProvider = JwkProviderBuilder(issuer)
+        .cached(10, 24, TimeUnit.HOURS)
+        .rateLimited(10, 1, TimeUnit.MINUTES)
+        .build()
+    install(Authentication) {
+        jwt("auth-jwt") {
+            realm = myRealm
+            verifier(jwkProvider, issuer) {
+                acceptLeeway(3)
+            }
+        }
+    }
     // endregion
 
     install(Authentication) {
@@ -57,6 +87,20 @@ fun Application.configureAuthentication() {
                 }
             }
         }
+
+        jwt("auth-jwt") {
+            realm = jwtRealm
+            validate { credential ->
+                if (credential.payload.getClaim("username").asString() != "") {
+                    JWTPrincipal(credential.payload)
+                } else {
+                    null
+                }
+            }
+            challenge { defaultScheme, realm ->
+                call.respond(HttpStatusCode.Unauthorized, "Token is not valid or has expired")
+            }
+        }
     }
 
     routing {
@@ -73,6 +117,29 @@ fun Application.configureAuthentication() {
         authenticate("auth-form") {
             post("/auth-form") {
                 call.respondText("Hello, ${call.principal<UserIdPrincipal>()?.name}!")
+            }
+        }
+        authenticate("auth-jwt") {
+            post("/auth-jwt") {
+                val user = call.receive<User>()
+                // Check username and password
+                // ...
+                val publicKey = jwkProvider.get("6f8856ed-9189-488f-9011-0ff4b6c08edc").publicKey
+                val keySpecPKCS8 = PKCS8EncodedKeySpec(Base64.getDecoder().decode(privateKeyString))
+                val privateKey = KeyFactory.getInstance("RSA").generatePrivate(keySpecPKCS8)
+                val token = JWT.create()
+                    .withAudience(audience)
+                    .withIssuer(issuer)
+                    .withClaim("username", user.username)
+                    .withExpiresAt(Date(System.currentTimeMillis() + 60000))
+                    .sign(Algorithm.RSA256(publicKey as RSAPublicKey, privateKey as RSAPrivateKey))
+                call.respond(hashMapOf("token" to token))
+            }
+            get("/auth-jwt/hello") {
+                val principal = call.principal<JWTPrincipal>()
+                val username = principal!!.payload.getClaim("username").asString()
+                val expiresAt = principal.expiresAt?.time?.minus(System.currentTimeMillis())
+                call.respondText("Hello, $username! Token is expired at $expiresAt ms.")
             }
         }
     }
